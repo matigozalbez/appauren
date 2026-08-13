bf680fd6c1350c4ab63f407fb344e92c50a05433
matigozalbez <matiasgozalbez@gmail.com>
2026-08-12 22:39:47 -0300

    change vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
         display: 'standalone',
         start_url: '/',
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: '/auren-isotipo.png', sizes: '192x192', type: 'image/png' },
+          { src: '/auren-isotipo.png', sizes: '512x512', type: 'image/png' },
         ],
       },
     }),
