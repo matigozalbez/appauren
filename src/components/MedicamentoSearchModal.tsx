049580f8e77567382c6825d56ced2f4de55ae99e
matigozalbez <matiasgozalbez@gmail.com>
2026-08-14 05:44:02 -0300

    Change input medicamentos

--- src/components/MedicamentoSearchModal.tsx
+++ src/components/MedicamentoSearchModal.tsx
@@ -103,6 +103,10 @@ export default function MedicamentoSearchModal({ isOpen, onClose }: Props) {
               onChange={(e) => setQuery(e.target.value)}
               placeholder="Ej: Ibuprofeno"
               className="w-full pl-9 pr-3 py-2.5 rounded-lg bg-slate-50 border border-slate-200 text-sm outline-none focus:border-[#0F1E3D]"
+              autoCapitalize="off"
+              autoComplete="off"
+              spellCheck="false"
+              autoCorrect="off"
             />
           </div>
         </div>
