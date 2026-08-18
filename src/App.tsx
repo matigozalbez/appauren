0d1b93612b393a5fda9b38d95e8b2bb8509045cd
matigozalbez <matiasgozalbez@gmail.com>
2026-08-18 11:21:56 -0300

    soy un noob

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
  const showNav = location.pathname !== '/' && location.pathname !== '/primer-ingreso';
 const [isStandalone, setIsStandalone] = useState<boolean | null>(null);
   const navigate = useNavigate();
-/*
+
 useEffect(() => {
   
   const check =
@@ -39,7 +39,7 @@ if (isStandalone === null) {
 if (!isStandalone) {
   return <InstalarApp />;
 }
-*/
+
     const handleLogout = async () => {
       localStorage.removeItem("auren_dni");
       localStorage.removeItem("auren_planes");
