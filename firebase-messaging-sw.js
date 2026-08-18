1231ff8ac3b1fd04af2a1862e352aaaa7c823754
matigozalbez <matiasgozalbez@gmail.com>
2026-08-18 12:36:00 -0300

    godigodigodi

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCVUZaRCgP_6CxqirvkSccAOoFR1DTNOG8",
+  authDomain: "uberblog-bb8eb.firebaseapp.com",
+  projectId: "uberblog-bb8eb",
+  storageBucket: "uberblog-bb8eb.firebasestorage.app",
+  messagingSenderId: "335604735631",
+  appId: "1:335604735631:web:b3e04bdda6305fdbf11afa"
 });
+
 
 const messaging = firebase.messaging();
 
