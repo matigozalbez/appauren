11feadc0375df4d21c490623156aa6be0db9d39a
matigozalbez <matiasgozalbez@gmail.com>
2026-08-23 11:45:30 -0300

    asdadas fuck

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -119,7 +119,7 @@ useEffect(() => {
   onOpenNotifications={() => setNotisOpen(true)}
   unreadCount={unreadCount}
 />
-        <div className="flex-1 pb-20 overflow-y-auto scrollbar-hide w-full">
+        <div className="flex-1 pb-20 overflow-y-auto scrollbar-hide w-full style={{ overscrollBehaviorY: 'none' }">
         {/* Bloque azul con la panza hacia arriba (curva inferior normal y superior recta o viceversa según el flujo visual) */}
 <div className="relative bg-gradient-to-br from-[#0F1E3D] via-[#152953] to-[#0A1429] px-6 pt-6 pb-20 rounded-t-3xl shadow-xl"> 
   <div className="flex items-center justify-between">
