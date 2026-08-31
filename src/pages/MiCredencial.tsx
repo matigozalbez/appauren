e8444a168c7da7558b22304632ce5923c986feb4
matigozalbez <matiasgozalbez@gmail.com>
2026-08-31 20:21:10 -0300

    change credential

--- src/pages/MiCredencial.tsx
+++ src/pages/MiCredencial.tsx
@@ -86,7 +86,6 @@ export default function MiCredencial() {
 
   return (
     <div className={`min-h-screen-safe bg-gradient-to-b from-[#FDFBF7] via-[#FBF6EC] to-[#F5EAD2] pb-10 ${animationClass}`}>
-      <div className="min-h-screen-safe bg-[#FDFBF7] pb-10">
         {/* Header */}
         <div className="pt-8 pb-6 px-6 border-b border-slate-200 bg-gradient-to-br from-[#0F1E3D] via-[#152953] to-[#0A1429]">
           <h1 className="text-xl font-bold tracking-tight text-white flex items-center justify-between">
@@ -341,7 +340,6 @@ export default function MiCredencial() {
           </div>
         </div>
 
-      </div>
     </div>
   );
 }
